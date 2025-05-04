8a5bf66ab10754445d5245b9c4ecbafd1c5ca08c
Francisco Eduardo <franciscoeduardo@MacBook-Air-de-Francisco.local>
2025-05-03 21:39:12 -0300

    created upload widget upload items, dropzone and upload list

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,12 @@ export default {
       opacity: {
         2: 0.02,
       },
+      fontSize: {
+        xxs: '0.625rem', // 10px
+      },
       boxShadow: {
         shape: '0px 8px 8px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 2px 2px rgba(0, 0, 0, 0.1), 0px 0px 0px 1px rgba(0, 0, 0, 0.1), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.03), inset 0px 1px 0px rgba(255, 255, 255, 0.03);',
-        'shapeContent': '0px 0px 0px 1px rgba(0, 0, 0, 0.25), inset 0px 1px 0px rgba(255, 255, 255, 0.02), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.02);'
+        'shape-content': '0px 0px 0px 1px rgba(0, 0, 0, 0.25), inset 0px 1px 0px rgba(255, 255, 255, 0.02), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.02);'
       },
     },
   },

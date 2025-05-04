5e092bd5f5e5e9ba2a3f283a84a4be8ea9c6152c
Francisco Eduardo <franciscoeduardo@MacBook-Air-de-Francisco.local>
2025-05-04 17:42:49 -0300

    created animations

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,3 +1,4 @@
+import { isDragActive, motion } from 'motion/react'
 import { useDropzone } from 'react-dropzone';
 import CircularProgressBar from './ui/circular-progress-bar';
 
@@ -15,8 +16,13 @@ export function UploadWidgetDropzone() {
     },
   });
 
-  return(
-    <div className="px-3 flex flex-col gap-3">
+  return( 
+    <motion.div 
+      className="px-3 flex flex-col gap-3"
+      initial= {{ opacity: 0 }}
+      animate= {{ opacity: 1 }}
+      transition={{duration: 1}}
+    >
       <div
         data-active={isDragActive}
         className='cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500 data-[active=true]:text-indigo-400'
@@ -38,6 +44,6 @@ export function UploadWidgetDropzone() {
       </div>
 
       <span className='text-xxs text-zinc-400'>Only PNG and JPG files are supported.</span>
-    </div>
+    </motion.div>
   )
 }
